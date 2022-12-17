ecb737dd52e11ff0c337ef3a80a24456c265617c
ilyas <ilyas.kulnazarov@nat.kz>
2022-12-17 17:48:25 +0500

    Увеличение максимальных символов в вопросах и ответах

--- api/models.py
+++ api/models.py
@@ -17,7 +17,7 @@ class Topics(models.Model):
 
 class Questions(models.Model):
     variant_number = models.ForeignKey(Variants, on_delete=models.CASCADE, verbose_name="Variant")
-    question_text = models.CharField("questionText", max_length=255)
+    question_text = models.CharField("questionText", max_length=1000)
     topic = models.ForeignKey(Topics, on_delete=models.CASCADE, verbose_name="Topic")
 
     def __str__(self):
@@ -27,7 +27,7 @@ class Questions(models.Model):
 class Answers(models.Model):
     question = models.ForeignKey(Questions, on_delete=models.CASCADE)
     right_answer = models.BooleanField("rightAnswer")
-    answer_text = models.CharField("answerText", max_length=500)
+    answer_text = models.CharField("answerText", max_length=1000)
 
     def __str__(self):
         return self.answer_text
